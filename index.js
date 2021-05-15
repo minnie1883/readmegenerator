d8a4d196c6a41196542758a4bd06c133f7975282
Stephanie Rice <swright9616@yahoo.com>
2021-05-15 11:22:25 -0500

    update inputs

--- index.js
+++ index.js
@@ -4,10 +4,70 @@
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require("./utils/generateMarkdown");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your README title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Description of the project:',
+    },
+    {
+      type: 'input',
+      name: 'install',
+      message: 'Install instructions for the project:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How do you use the application?',
+    },
+    {
+      type: 'input',
+      name: 'list',
+      message: 'Choose a license for the README',
+      choices : ["Apache", "Boost", "BSD", "IBM", "Pearl", "MIT"],
+    },
+    {
+      type: 'input',
+      name: 'contribtions',
+      message: 'Add other contributors to this project',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter test instructions:'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your Github username',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:'
+    },
+  ]);
+};
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+}
 
 // Function call to initialize app
 init();
